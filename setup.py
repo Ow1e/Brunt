577426972a228d76d6ed858c8c1968abf9d61fdd
Owen Shaule <owen.shaule@gmail.com>
2022-10-01 09:26:05 -0400

    add setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+  
+  
+setuptools.setup(
+    name="Brunt",
+    version="0.1",
+    author="Owen Shaule",
+    author_email="hello@cyclone.biz",
+    packages=["brunt"],
+    description="A better and slightly more cool web interface for Flask",
+    long_description="SocketIO + Flask, without the annoyance",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ow1e/Brunt",
+    license='MIT',
+    python_requires='>=3.8',
+    install_requires=["Flask", "Flask-SocketIO", "simple-websocket"]
+)
